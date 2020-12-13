6b02614aeb55651cd5b732f84612af56f4fc1aa2
Alice <bigidiki143@gmail.com>
2020-12-13 19:08:31 +0500

    switch case

---
+++ hanoi_tower/hanoi_tower/HanoiTower.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hanoi_tower
+{
+    class HanoiTower
+    {
+        public int[] a = new int[5] { 4, 3, 2, 1, 3 };
+        public int[] b = new int[5] { 0, 0, 0, 0, 0 };
+        public int[] c = new int[5] { 0, 0, 0, 0, 0 };
+
+
+
+        public HanoiTower()
+        {
+
+        }
+        public bool move(int[] x, int[] y)
+        {
+            if (x[x[4]] > y[y[4]] && y[y[4]]!=0)
+            {
+                Console.WriteLine("Операцию выполнить невозможно");
+                return false;
+            }
+            else
+            {
+                swap(ref x[x[4]], ref y[y[4]]);
+                x[4]--;
+                y[4]++;
+                return true;
+            }
+
+        }
+
+    
+        public void Display()
+        {
+            ConsoleColor cc = Console.ForegroundColor;
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(a[i] + "                     ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(b[i] + "                     ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(c[i] + "                     ");
+                Console.WriteLine();
+            }
+            Console.WriteLine(  " "  );
+        }   
+
+        public void swap(ref int x,ref int y)
+        {
+            int temp = y;
+            y = x;
+            x = temp;
+        }
+
+    }
+}
---
+++ hanoi_tower/hanoi_tower/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hanoi_tower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HanoiTower ht = new HanoiTower();
+            //string a = "Пользаватель выбрал а";
+            //string b = "Пользаватель выбрал б";
+            //string c = "Пользаватель выбрал ц";
+            while (true)
+            {
+                Console.WriteLine("Введите пожалуйста букву a,b,c");
+                string str = Console.ReadLine();
+                if (str == "q") { break; }
+                
+                switch (str)
+                {
+                    case "a":
+                        Console.WriteLine("Вы выбрали массив a,выберите b или с");
+                        if (str == "b")
+                        {
+                            goto case "b";
+                        }
+                        else if (str == "c")
+                        {
+                            goto case "c";
+                        }
+                        break;
+                       
+                    case "b":
+                        Console.WriteLine("Вы выбрали массив b");
+                        ht.move(ht.a, ht.b);
+                        ht.Display();
+                        break;
+                    case "c":
+                        Console.WriteLine("Вы выбрали массив c");
+                        ht.move(ht.a, ht.c);
+                        ht.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Невозможно выполнить операцию");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
